ae6bf1380e385bb1c9112bb5e6a7f60bb917885f
skwidhubz <tommaxwellparker@gmail.com>
2023-03-02 21:47:53 +1100

    project commit

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -4,7 +4,7 @@ const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-  res.send("<h1>Wrong Route .. ya silly duffer</h1>")
+  res.send("<h1>Wrong Route. Server is connected and available</h1>")
 });
 
 module.exports = router;
